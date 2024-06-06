ccefb4f1fecbd1085516da88ede8ee2f8b63188d
rnrabi <rnrabi913@gmail.com>
2024-06-06 09:59:19 +0600

    my cart delete api is set up ok

--- index.js
+++ index.js
@@ -126,6 +126,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/myCarts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartsCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
 
 

fa5b12f3fb18a2a1b7d8107ec8f86d6486200094
rnrabi <rnrabi913@gmail.com>
2024-06-10 20:09:52 +0600

    booking api is ok

--- index.js
+++ index.js
@@ -130,6 +130,19 @@ async function run() {
             res.send({ roll });
         })
 
+        app.get('/booking', async (req, res) => {
+            const result = await bookingCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/booking/:email', async (req, res) => {
+            const customarEmail = req.params.email;
+            const query = { 'customar.email': customarEmail }
+            const result = await bookingCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
         // payment intent**************
         app.post("/create-payment-intent", async (req, res) => {
             const { price } = req.body;
@@ -139,7 +152,7 @@ async function run() {
             console.log(priceInCents)
             // Create a PaymentIntent with the order amount and currency
             const paymentIntent = await stripe.paymentIntents.create({
-                amount: 100,
+                amount: priceInCents,
                 currency: "usd",
                 // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
                 automatic_payment_methods: {
